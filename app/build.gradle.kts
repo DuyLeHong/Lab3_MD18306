9429599ce892e4639651379e7bb47fa448d7bb88
Duy Le <lehongduy.bk@gmail.com>
2024-05-16 10:55:53 +0700

    Bai tap 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -59,6 +61,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
